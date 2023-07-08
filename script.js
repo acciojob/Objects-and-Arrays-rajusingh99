7ce68bad3d8a805854da379de6142c1685722639
Raju Singh <94919614+rajusingh99@users.noreply.github.com>
2023-07-08 11:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// const players = ["John", "Bob", "Alice", "Poppy"];
+
+// const person = {
+//   name: "John Doe",
+//   age: 80,
+// };
+
+// // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+
+
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
+
+
+
+
 const players = ["John", "Bob", "Alice", "Poppy"];
 
 const person = {
@@ -5,13 +24,13 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-const team= players;
-const team1= [...players];
-const cap1 = {....person};
+const team = players;
+const team1 = [...players];
+const cap1 = { ...person };
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
